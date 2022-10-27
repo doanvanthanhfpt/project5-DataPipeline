bbd090b4e3a05baa3bb5135663ede6665427fc0d
doanvanthanh <105765160+doanvanthanhfpt@users.noreply.github.com>
2022-10-27 08:49:06 +0700

    Pending for test

--- plugins/operators/data_quality.py
+++ plugins/operators/data_quality.py
@@ -8,15 +8,38 @@ class DataQualityOperator(BaseOperator):
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
-                 *args, **kwargs):
+                # Define your operators params (with defaults) here
+                # Example:
+                # conn_id = your-connection-name
+                redshift_conn_id="",
+                aws_credentials_id="",
+                all_tables="",
+                *args, **kwargs):
 
         super(DataQualityOperator, self).__init__(*args, **kwargs)
         # Map params here
         # Example:
         # self.conn_id = conn_id
+        self.redshift_conn_id = redshift_conn_id,
+        self.aws_credentials_id = aws_credentials_id,
+        self.all_tables = all_tables,
 
     def execute(self, context):
-        self.log.info('DataQualityOperator not implemented yet')
+        # self.log.info('DataQualityOperator not implemented yet')
+        self.log.info('Starting DataQualityOperator implementation')
+
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+        
+        for tbl in range(len(self.all_tables)):
+            table = list(self.all_tables.keys())[tbl][0]
+            field = list(self.all_tables.values())[tbl][1]
+            
+            # Check tables - have rows
+            check_table_sql = f"SELECT Count(*) FROM {table}"
+            rows = redshift.get_first(check_table_sql) 
+            self.log.info(f'Table: {table} has {rows} rows')
+            
+            # Check fields with null entries
+            check_table_sql = f"SELECT Count(*) FROM {table} where {field} IS NULL"
+            rows = redshift.get_first(check_table_sql) 
+            self.log.info(f'Field: "{field}" in table: {table} has {rows} NULL rows')
--- plugins/operators/load_dimension.py
+++ plugins/operators/load_dimension.py
@@ -8,15 +8,37 @@ class LoadDimensionOperator(BaseOperator):
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
-                 *args, **kwargs):
+                # Define your operators params (with defaults) here
+                # Example:
+                # conn_id = your-connection-name
+                redshift_conn_id="",
+                aws_credentials_id="",
+                table="",
+                action="append",
+                load_dim_sql="",
+                *args, **kwargs):
 
         super(LoadDimensionOperator, self).__init__(*args, **kwargs)
         # Map params here
         # Example:
         # self.conn_id = conn_id
+        self.redshift_conn_id = redshift_conn_id,
+        self.aws_credentials_id = aws_credentials_id,
+        self.table = table,
+        self.action = action,
+        self.load_dim_sql = load_dim_sql,
 
     def execute(self, context):
-        self.log.info('LoadDimensionOperator not implemented yet')
+        # self.log.info('LoadDimensionOperator not implemented yet')
+        self.log.info('Starting LoadDimensionOperator')
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+
+        if self.action == 'truncate':
+            self.log.info("Truncating table {}".format(self.table))
+            redshift.run("TRUNCATE TABLE {}".format(self.table))
+
+        self.log.info("Inserting data from StagingTables into DimTables")
+        insert_table_sql = "INSERT INTO {} {}".format(self.table, self.insert_table_sql)
+        redshift.run(insert_table_sql)
+
+        self.log.info("Done: Inserting data on {}, {} loaded.".format(self.table))
